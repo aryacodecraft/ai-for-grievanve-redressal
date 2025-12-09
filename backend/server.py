87af7bf359972f19ec59045f79c982b09da9ebc3
unknown <imshlosktiwari7@gmail.com>
2025-12-09 15:41:20 +0530

    changed server.py (port no and debug=false)

--- backend/server.py
+++ backend/server.py
@@ -397,5 +397,5 @@ def submit_grievance():
 
 # --- 7. Start Server ---
 if __name__ == "__main__":
-    PORT = int(os.getenv("PORT", 5000))
-    app.run(host='0.0.0.0', port=PORT, debug=True)
+    PORT = int(os.getenv("PORT", 10000))
+    app.run(host='0.0.0.0', port=PORT, debug=false)
